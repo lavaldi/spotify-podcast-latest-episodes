e9949e7a6e243559d88613f04fd06473a1d2a7f5
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-10-30 21:54:35 -0500

    get session from server side

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import { getSession } from "next-auth/react";
 import Layout from "../components/layout";
 
-export default function Page() {
+export default function Page({ session }) {
   return (
     <Layout>
       <h1>Spotify Auth Example</h1>
-      <p>Nothing to see here yet 😜</p>
+      {session ? <p>Yay! 🎉</p> : <p>Nothing to see here yet 😜</p>}
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context)
+    }
+  }
 }
